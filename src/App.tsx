78686de280a12470b31acb44322f4c2c5ae6b04e
Jean-Marc <jmw_fr@hotmail.com>
2021-03-13 15:33:56 +0100

    Ajout de rxjs-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./styles.css";
-import { useObservable } from "rxjs-hooks";
+import { useObservable, useEventCallback } from "rxjs-hooks";
 import { interval } from "rxjs";
 import { map } from "rxjs/operators";
 
 export default function App() {
   const value = useObservable(() => interval(500).pipe(map((val) => val * 3)));
+
+  const [clickCallback, [description, x, y]] = useEventCallback(
+    (event$) =>
+      event$.pipe(
+        map((event: any) => {
+          return [event.target.innerHTML, event.clientX, event.clientY];
+        })
+      ),
+    ["nothing", 0, 0]
+  );
 
   return (
     <div className="App">
@@ -12,6 +22,14 @@ export default function App() {
       <h2>Start editing to see some magic happen!</h2>
 
       <h1>Incremental number: {value}</h1>
+
+      <h1>
+        click position: {x}, {y}
+      </h1>
+      <h1>"{description}" was clicked.</h1>
+      <button onClick={clickCallback}>click me</button>
+      <button onClick={clickCallback}>click you</button>
+      <button onClick={clickCallback}>click him</button>
     </div>
   );
 }
